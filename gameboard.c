fe9ec4afab986cb1066b9bcb765e101888a8881a
uxd2486 <uxd2486@rit.edu>
2019-01-26 18:24:20 -0500

    Implemented initialise_board() in gameboard.c, and added documentation for the functions in gameboard.h

--- gameboard.c
+++ gameboard.c
@@ -5,5 +5,18 @@
 // Author : Utkarsh Dayal
 //
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "gameboard.h"
+
+//Functions(extensive documentation in gameboard.h)
+
+//
+// Creates the gameboard
+//
+char **initialise_board(){
+    char **board = calloc(9, sizeof(char) ); //the board is 3x3, hence 9 members
+    return board;
+}
+
+
--- gameboard.h
+++ gameboard.h
@@ -9,12 +9,37 @@
 
 //Function declarations
 
+//
+// initialise_board() - creates the gameboard as a 2-D array of characters and
+//                      returns it. Initially, it will be populated with the
+//                      null character
+//
+// return - the gameboard
+//
 char **initialise_board();
 
+//
+// print_board() - prints the current state of the board to stdin
+//
 void print_board(char **gameboard);
 
+//
+// mark_board() - changes the board at the location specified by row and col
+//                to the symbol given by mark.
+//                row and col must be greater than or equal to 0
+//                mark must be either 'x' or 'o'
+//
+// mark - the mark to be made on the board('x' or 'o')
+// row - int that specifies the row for the mark to be made
+// col - int that specifies the column for the mark to be made
+//
+// return - the gameboard, after making the change
+//
 char **mark_board(char mark, int row, int col);
 
+//
+// free_board() - frees the memory where the gameboard is stored
+//
 void free_board(char **gameboard);
 
 #endif //TIC_TAC_TOE_GAMEBOARD_H
