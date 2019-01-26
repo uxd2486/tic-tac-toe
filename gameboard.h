a81af744a5cca419969cf05c2fc6cee127c557c4
uxd2486 <uxd2486@rit.edu>
2019-01-26 18:08:38 -0500

    Added some basic function declarations for the gameboard to gameboard.h

--- gameboard.h
+++ gameboard.h
@@ -7,4 +7,14 @@
 #ifndef TIC_TAC_TOE_GAMEBOARD_H
 #define TIC_TAC_TOE_GAMEBOARD_H
 
+//Function declarations
+
+char **initialise_board();
+
+void print_board(char **gameboard);
+
+char **mark_board(char mark, int row, int col);
+
+void free_board(char **gameboard);
+
 #endif //TIC_TAC_TOE_GAMEBOARD_H
