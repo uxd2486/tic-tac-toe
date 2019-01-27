e594d74f3d4895f9a7d50aaef098273e7fe78b8b
uxd2486 <uxd2486@rit.edu>
2019-01-26 19:02:54 -0500

    Implemented print_board(), as well as print_horizontal_lines(), which is a helper function for print_board()

--- gameboard.c
+++ gameboard.c
@@ -9,6 +9,21 @@
 
 #include "gameboard.h"
 
+//Private functions
+
+//
+// print_horizontal_lines() - only used to print out the gameboard in a neat, readable format
+//                            prints out the following:
+//                                      _ _ _
+//
+//                            followed by a newline character
+void print_horizontal_lines(){
+    for (int i = 0; i < 3; i++){
+        printf("_ ");
+    }
+    printf("\n");
+}
+
 //Functions(extensive documentation in gameboard.h)
 
 //
@@ -17,6 +32,19 @@
 char **initialise_board(){
     char **board = calloc(9, sizeof(char) ); //the board is 3x3, hence 9 members
     return board;
+}
+
+void print_board(char ** gameboard){
+    for (int i = 0; i < 3 ; i++){
+        for (int j = 0; j < 3; j++) {
+            printf("%c ", gameboard[i][j]);
+            if (j == 2){
+                print_horizontal_lines();
+            } else{
+                printf("|");
+            }
+        }
+    }
 }
 
 
